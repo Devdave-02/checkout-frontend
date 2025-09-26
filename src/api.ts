4e6d5e6fe88b80c82faabf9e35b25fa3c377b530
Devdave-02 <davidokanla69@gmail.com>
2025-09-26 19:11:42 +0100

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // later replace with deployed backend URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
 });
 
 export default api;
+
+
